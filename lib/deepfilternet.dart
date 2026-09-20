dcf3aa518b525d701ab878d75815213972b72bba
Fahad <44726129+DaPotatoMan@users.noreply.github.com>
2026-09-20 20:37:31 +0600

    patch

--- lib/deepfilternet.dart
+++ lib/deepfilternet.dart
@@ -2,4 +2,6 @@ export 'src/bindings.dart'
     if (dart.library.io) 'src/bindings_native.dart'
     if (dart.library.js_interop) 'src/bindings_web.dart';
 export 'src/shared.dart';
-export 'src/worker.dart' if (dart.library.io) 'src/worker_vm.dart' if (dart.library.js_interop) 'src/worker_web.dart';
+export 'src/worker.dart'
+    if (dart.library.io) 'src/worker_native.dart'
+    if (dart.library.js_interop) 'src/worker_web.dart';
